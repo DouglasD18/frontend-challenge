b9776e1958f81e524f0d6fe7c9c87f3b8846a450
Douglinhas <daguiaralcantara@gmail.com>
2023-11-16 16:52:08 -0300

    refactore: configure images in the next configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mks-sistemas.nyc3.digitaloceanspaces.com'
+      },
+    ],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
